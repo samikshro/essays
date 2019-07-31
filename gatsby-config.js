ff26ea72c17e4f03987adab7cf2182bcf615a034
Samik Shrotriya <ss2659@cornell.edu>
2019-07-31 17:15:29 -0400

    add public folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     title: `Gatsby Starter Blog`,
     author: `Kyle Mathews`,
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://samikshro.github.io`,
     social: {
       twitter: `kylemathews`,
     },
